d9a5207b4c07d745dbe32606cf1771de68e534d7
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-11-16 13:35:15 +0600

    DOM hocca bowser a kaj kora

---
+++ explore.js
@@ -0,0 +1,9 @@
+const name = 15;
+const student = {
+    name: 'Nizam', friend: 'kajol', job: 'Business',
+    makeMovie: function (name) {
+        console.log(name);
+    },
+}
+student.makeMovie('shevly')
+// document.getElementById('name');
