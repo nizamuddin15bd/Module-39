b6069e6a6200798d212bc78e3794b2d283c87611
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-11-16 22:44:56 +0600

    history.js

---
+++ history.js
@@ -0,0 +1,10 @@
+// 1. history.back(); // amra back a jaita pare
+// 2. history.forword ; // samne jaita pare
+// 3. history.go(0);      // amra number deya bola deta pare kothai jabe 
+// ar 0 dela refres hoba
+// 4.histoy.length dela koita history aca ta jana jabe
+// 5. search  browser history api
+
+
+
+// history hocca 1ta tab ar modda aga kothi kothi celam abar pecona thakla samne kothi jabo ta history maddoma kora jai
