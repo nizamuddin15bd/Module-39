0e69ae505a9c428a4f976d85e1e22e4256f7ea5c
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-11-18 01:06:18 +0600

    shooping cart end

--- shopping.js
+++ shopping.js
@@ -1,12 +1,22 @@
+const displayLocalStorageCart = () => {
+    const cart = getCart();
+    for (const name in cart) {
+        displayProduct(name);
+    }
+}
+
 const addItem = () => {
     const namField = document.getElementById('product-name');
     const name = namField.value;
+    if (!name) {
+        return;
+    }
     // display in the ui
     displayProduct(name);
     // add to localstorage
     addproductToCart(name);
 
-    namField.value= '';
+    namField.value = '';
 }
 const displayProduct = name => {
     const ul = document.getElementById('products');
@@ -16,21 +26,32 @@ const displayProduct = name => {
 }
 
 const getCart = () => {
-   const cart =  localStorage.getItem('cart')
-   let cartObj;
-   if(cart){
+    const cart = localStorage.getItem('cart')
+    let cartObj;
+    if (cart) {
         cartObj = JSON.parse(cart);
-   }
-   else{
-       cartObj = {};
-   }
-   return cartObj;
+    }
+    else {
+        cartObj = {};
+    }
+    return cartObj;
 }
 
 const addproductToCart = name => {
     const cart = getCart();
-    cart[name] = 1;
+    if (cart[name]) {
+        cart[name] = cart[name] + 1;
+    }
+    else {
+        cart[name] = 1;
+    }
     // console.log(cart);
     const cartStringigied = JSON.stringify(cart);
     localStorage.setItem('cart', cartStringigied);
 }
+const placeOrder = () => {
+    document.getElementById('products').textContent = '';
+    localStorage.removeItem('cart');
+}
+
+displayLocalStorageCart();
